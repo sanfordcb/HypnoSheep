dd95a3664e8f4c83f0732d2702f243a20d51bfaf
Thr1ve <fict1on.buhler@gmail.com>
2016-02-18 20:56:12 -0500

    add api routes to server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,23 @@
 var express = require('express');
-var app = express();
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var routes = require('./routes.js');
+
+var db = require('./dbConfig.js');
+
+var app = express();
 
 var port = process.env.PORT || 8080;
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 app.use(morgan('dev'));
+
+app.use('/api', routes);
+
 app.use(express.static('public'));
 
 app.listen(port, function(){
   console.log('Listening on port', port);
 });
-
