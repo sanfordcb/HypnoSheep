da8ac7927914896543bfe5700b851b570d899e88
Thr1ve <fict1on.buhler@gmail.com>
2016-02-19 17:42:37 -0500

    add routes to get links and get links by project

--- server/links/linkController.js
+++ server/links/linkController.js
@@ -1,10 +1,35 @@
 var Link = require('./linkModel.js');
+var Project = require('../projects/projectModel.js');
 
 module.exports = {
+  // Get all the links
   getLinks: function(req, res) {
-    res.end('get links');
+    Link.find()
+    .then(function(links) {
+      res.json(links);
+    });
   },
 
+  // Get all links for a specific project
+  getLinksByProject: function(req, res) {
+    var projectId = req.params.projectId;
+    Project.findOne({_id: projectId})
+    .then(function(project) {
+      if (!project) {
+        return res.end('project not found');
+      }
+      Link.find({project: project._id})
+      .then(function(links) {
+        res.json(links);
+      })
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.end(err);
+    })
+  },
+
+  // TODO:
   addLink: function(req, res) {
     res.end('add link');
   }
