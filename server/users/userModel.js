349b1529303837289e674956c5e9de089fb3241c
Thr1ve <fict1on.buhler@gmail.com>
2016-02-19 23:18:15 -0500

    add auth endpoints

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -1,8 +1,38 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var SALT_WORK_FACTOR = 10;
 
 var UserSchema = new mongoose.Schema({
-  username: String,
-  password: String
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword) {
+  var savedPassword = this.password;
+  return new Promise(function(resolve, reject) {
+    bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch){
+      if (err) {
+        reject(err);
+      } else {
+        resolve(isMatch);
+      }
+    })
+  })
+}
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+  var salt = bcrypt.genSaltSync(SALT_WORK_FACTOR);
+  var hash = bcrypt.hashSync(user.password, salt);
+  user.password = hash;
+  next();
 });
 
 module.exports = mongoose.model('User', UserSchema);
