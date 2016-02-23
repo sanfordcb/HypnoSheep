6e45e7482e93a65a77f42001b970ee034baaf27c
Thr1ve <fict1on.buhler@gmail.com>
2016-02-23 14:28:01 -0500

    test ajax calls to server in dev

--- app/main.js
+++ app/main.js
@@ -1,7 +1,19 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import request from 'superagent/lib/client';
 
 class App extends React.Component {
+  componentDidMount() {
+    request
+      .get('/api/projects')
+      .end(function(err, res) {
+        if (err) {
+          console.log(err)
+        }
+        console.log(res);
+      });
+  }
+
   render() {
     return (
       <div>
