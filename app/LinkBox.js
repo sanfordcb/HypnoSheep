cf759b6f661a9fa47db46944d2db4a0ff4a22ca1
Thr1ve <fict1on.buhler@gmail.com>
2016-02-23 19:35:21 -0500

    fix LinkBox

--- app/LinkBox.js
+++ app/LinkBox.js
@@ -5,53 +5,43 @@ var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 
 var LinkBox = React.createClass({
-  loadUrl: function(){
-    var url = window.location.href;
-    var id = url.split('/').pop();
-    return id;
+  getInitialState: function() {
+    return {
+      data: []
+    };
   },
-  loadLinksFromServer: function() {
-    $.ajax({
-      url: '/api/links/' + this.state.projectId,
-      dataType: 'text',
-      cache: false,
-      type: 'GET',
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error('/api/links', status, err.toString());
-      }.bind(this)
-    });
-  },
+
   handleLinkSubmit: function(link) {
-    var links = this.state.data;
-    var newLinks = links.concat([link]);
-    this.setState({data: newLinks});
+    link.projectId = this.props.params.id;
     $.ajax({
       url: '/api/links',
       contentType: 'application/json',
       type: 'POST',
-      data: link,
-      success: function(data) {
-        this.setState({data: data});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        this.setState({data: links});
+      data: JSON.stringify(link),
+      success: (data) => {
+        this.setState({data: this.state.data.concat(data)});
+      },
+      error: (xhr, status, err) => {
         console.error('/api/projects', status, err.toString());
-      }.bind(this)
+      }
     });
   },
-  getInitialState: function() {
-    return {projectId:0, data: [{url: 'http://www.google.com', _id: 13123123}, {url: 'http://www.bing.com', _id: 131232123}, {url: 'http://www.yahoo.com', _id: 131231253}]};
-  },
-  componentDidMount: function() {
-    var projectId = this.loadUrl();
-    this.setState({projectId: projectId});
 
-    //this.loadLinksFromServer();
-    //setInterval(this.loadLinksFromServer, this.props.pollInterval);
+  componentDidMount: function() {
+    $.ajax({
+      url: '/api/links/' + this.props.params.id,
+      dataType: 'text',
+      cache: false,
+      type: 'GET',
+      success: (data) => {
+        this.setState({data: this.state.data.concat(JSON.parse(data))});
+      },
+      error: (xhr, status, err) => {
+        console.error('/api/links', status, err.toString());
+      }
+    });
   },
+
   render: function() {
     return (
       <div className="linkBox">
@@ -63,7 +53,6 @@ var LinkBox = React.createClass({
   }
 });
 
-
 var LinkForm = React.createClass({
   getInitialState: function() {
     return {url: ''};
@@ -73,12 +62,12 @@ var LinkForm = React.createClass({
   },
   handleSubmit: function(e) {
     e.preventDefault();
-    var link = this.state.link.trim();
+    var link = this.state.url.trim();
     if (!link) {
       return;
     }
-    this.props.onLinkSubmit({link: link});
-    this.setState({link: ''});
+    this.props.onLinkSubmit({url: link});
+    this.setState({url: ''});
   },
   render: function() {
     return (
@@ -86,7 +75,7 @@ var LinkForm = React.createClass({
         <input
           type="link"
           placeholder="Say something..."
-          value={this.state.link}
+          value={this.state.url}
           onChange={this.handleLinkChange}
         />
         <input type="submit" value="Post" />
@@ -114,7 +103,6 @@ var LinkList = React.createClass({
 
 var LinkItem = React.createClass({
   render: function() {
-    console.log('Link props, ', this.props);
     return (
       <div className="link">
         <Link to={`${this.props.link.url}`}>{this.props.link.url}</Link>
