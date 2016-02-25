9f3245abdb9692462d6569188acb320c9fe160ee
Thr1ve <fict1on.buhler@gmail.com>
2016-02-25 16:27:36 -0500

    add initial attempt at editable component

--- app/components/Resource.js
+++ app/components/Resource.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router';
+
+const Editable = ({ editable, content, callback, children }) => {
+  if (editable) {
+    return <input value={content} />;
+  }
+  return children;
+};
 
 // When creating a functional component with
 // React, props is passed as the first argument. Thus, we
 // can destructure the values we need from props
 //              v  v       v       v
-export default ({ resource, deleteResource }) => (
+export default ({ resource, deleteResource, toggleEdit, editable }) => (
   <div>
-    <a href={resource.url}>{resource.url}</a>
-    <button onClick={deleteResource}> delete </button>
+    <button onClick={toggleEdit}> Edit </button>
+    <Editable editable={editable} content={resource.url}>
+      <a href={resource.url}>{resource.url}</a>
+    </Editable>
+    {editable ? <button> Save </button> : <button onClick={deleteResource}> delete </button>}
   </div>
 );
