8835d3d6b2b4bf038bf054859eecd7e9cc453dfe
Thr1ve <fict1on.buhler@gmail.com>
2016-02-26 17:00:33 -0500

    update editable to use MaterialUI

--- app/components/Editable.js
+++ app/components/Editable.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import TextField from 'material-ui/lib/text-field';
+
 // NOTES:
 //  - This can only handle a single child.
 //  - That child must have a 'content' and an 'editKey' prop
@@ -7,8 +9,9 @@ const Editable = ({ editable, callback, children }) => {
   const { content, editKey } = children.props;
   if (editable) {
     return (
-      <input
+      <TextField
         defaultValue={content}
+        floatingLabelText={editKey}
         onChange={(event) => {
           callback(event.target.value, editKey);
         }}
