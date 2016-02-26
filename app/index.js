4418bf7b379087fae696b89566b4129a38bef8b3
Thr1ve <fict1on.buhler@gmail.com>
2016-02-26 13:45:27 -0500

    add material-ui and tap-event

--- app/index.js
+++ app/index.js
@@ -1,13 +1,24 @@
+// 3rd Party
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
+// Project
 import LandingPage from './LandingPage';
 import App from './App';
 import ProjectBox from './ProjectBox';
 import ProjectPage from './containers/ProjectPage';
 import SignupForm from './SignupForm';
 import SignIn from './SignIn';
+
+
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
 
 const routes = (
   <Router history={browserHistory}>
