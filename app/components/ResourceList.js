26a837b57c8cfba72f08ba8ce5a99f7c52f04563
Thr1ve <fict1on.buhler@gmail.com>
2016-02-25 21:33:26 -0500

    move ResourceList to separate module

---
+++ app/components/ResourceList.js
@@ -0,0 +1,35 @@
+const ResourceList = React.createClass({
+  // render returns an array of Resource components by mapping the resource objects
+  // stored in this.props.data
+  render() {
+    const { data, getResources } = this.props;
+
+    const resourceNodes = data.map((resource) => {
+      const deleteResource = () => {
+        request
+          .delete(`/api/resources/${resource._id}`)
+          .end((err, res) => {
+            if (err || !res.ok) {
+              console.log(err);
+            } else {
+              getResources();
+            }
+          });
+      };
+
+      return (
+        <ResourceContainer key={resource._id} resource={resource} deleteResource={deleteResource}>
+          {resource.url}
+        </ResourceContainer>
+      );
+    });
+
+    return (
+      <div className="resourceList">
+        {resourceNodes}
+      </div>
+    );
+  }
+});
+
+export default ResourceList;
