042313216672b6718b735334a167b799575e57fb
Thr1ve <fict1on.buhler@gmail.com>
2016-02-27 21:38:45 -0500

    remove extraneous files

--- app/Project.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { link } from 'react-router';
-
-export default ({ project, deleteProject }) => (
-  <div>
-    <a href={`/resources/${project._id}?user=${project.user}`}>{project.name}</a>
-    <button onClick={deleteProject}>delete</button>
-  </div>
-);
