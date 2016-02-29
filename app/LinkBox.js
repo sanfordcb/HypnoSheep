eab1428b1a6307099381e0182bb9ceee44e35b9a
Brian Moore <bmoorebrian53@gmail.com>
2016-02-29 15:00:37 -0500

    Live validation added

---
+++ app/LinkBox.js
@@ -0,0 +1,128 @@
+var $ = require('jquery');
+var marked = require('marked');
+var React = require('react');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
+import request from 'superagent';
+import Resource from './Resource.js';
+
+var LinkBox = React.createClass({
+  getInitialState: function() {
+    return {
+      data: []
+    };
+  },
+
+  getLinks: function() {
+    $.ajax({
+      url: `/api/links/${this.props.params.id}`,
+      dataType: 'text',
+      cache: false,
+      type: 'GET',
+      success: (data) => {
+        this.setState({ data: JSON.parse(data) });
+      },
+      error: (xhr, status, err) => {
+        console.error('/api/links', status, err.toString());
+      }
+    });
+  },
+
+  handleLinkSubmit: function(link) {
+    link.projectId = this.props.params.id;
+    $.ajax({
+      url: '/api/links',
+      contentType: 'application/json',
+      type: 'POST',
+      data: JSON.stringify(link),
+      success: (data) => {
+        this.setState({data: this.state.data.concat(data)});
+      },
+      error: (xhr, status, err) => {
+        console.error('/api/projects', status, err.toString());
+      }
+    });
+  },
+
+  componentDidMount: function() {
+    this.getLinks();
+  },
+
+  render: function() {
+    return (
+      <div className="linkBox">
+        <h1>Link</h1>
+        <LinkForm onLinkSubmit={this.handleLinkSubmit} />
+        <LinkList
+          data={this.state.data}
+          getLinks={this.getLinks}
+        />
+      </div>
+    );
+  }
+});
+
+var LinkForm = React.createClass({
+  getInitialState: function() {
+    return {url: ''};
+  },
+  handleLinkChange: function(e) {
+    this.setState({url: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var link = this.state.url.trim();
+    if (!link) {
+      return;
+    }
+    this.props.onLinkSubmit({url: link});
+    this.setState({url: ''});
+  },
+  render: function() {
+    return (
+      <form className="linkForm" onSubmit={this.handleSubmit}>
+        <input
+          type="link"
+          placeholder="Say something..."
+          value={this.state.url}
+          onChange={this.handleLinkChange}
+        />
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+});
+
+var LinkList = React.createClass({
+  render: function() {
+    const { data, getLinks } = this.props;
+
+    const linkNodes = data.map((link) => {
+      const deleteLink = () => {
+        request
+          .delete(`/api/links/${link._id}`)
+          .end((err, res) => {
+            if (err || !res.ok) {
+              console.log(err);
+            } else {
+              getLinks();
+            }
+          });
+      };
+
+      return (
+        <Resource key={link._id} link={link} deleteLink={deleteLink}>
+          {link.url}
+        </Resource>
+      );
+    });
+
+    return (
+      <div className="linkList">
+        {linkNodes}
+      </div>
+    );
+  }
+});
+
+module.exports = LinkBox;
--- app/components/ResourceFormModal.js
+++ app/components/ResourceFormModal.js
@@ -6,12 +6,32 @@ import Dialog from 'material-ui/lib/dialog';
 import TextField from 'material-ui/lib/text-field';
 
 const ResourceForm = React.createClass({
+  getInitialState() {
+    return {
+      url: '',
+      isValidUrl: null
+    }
+  },
+
   handleSubmit() {
     const url = this.url.getValue();
     const description = this.description.getValue();
-
     this.props.handleResourceSubmit({ url, description });
     this.props.closeModal();
+  },
+
+  urlChange(e) {
+    this.setState({ url: e.target.value })
+    this.validateUrl(e);
+  },
+
+  validateUrl(e) {
+    const url = e.target.value;
+    const urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
+    const isValidUrl = urlregex.test(url);
+    const urlError = isValidUrl ? null : "Invalid url, format should be http://www.google.com";
+    console.log('valid ', isValidUrl);
+    this.setState({ urlError, isValidUrl });
   },
 
   render() {
@@ -25,6 +45,7 @@ const ResourceForm = React.createClass({
         label="Submit"
         primary={true}
         keyboardFocused={true}
+        disabled={!this.state.isValidUrl}
         onTouchTap={() => {
           this.handleSubmit();
         }}
@@ -42,6 +63,8 @@ const ResourceForm = React.createClass({
         <TextField
           style={{ margin: 10 }}
           floatingLabelText={'Website'}
+          errorText={this.state.urlError}
+          onChange={this.urlChange}
           ref={node => {
             this.url = node;
           }}
