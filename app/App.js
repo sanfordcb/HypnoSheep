4a355286c38b5002a288ce434d291e37655dcacf
Thr1ve <fict1on.buhler@gmail.com>
2016-02-29 11:40:31 -0500

    redirect to signin if no jwt

--- app/App.js
+++ app/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import Header from './Header';
+import { browserHistory } from 'react-router';
 
 const App = React.createClass({
+  componentDidMount() {
+    if (!window.localStorage.jwt) {
+      browserHistory.push('/signin');
+    }
+  },
+
   render() {
     console.log('params in app: ', this.props.params);
     return (
