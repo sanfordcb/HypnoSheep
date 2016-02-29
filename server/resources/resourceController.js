10d69eb9719142fa40d8dc971f1a88346bbd2790
Brian Moore <bmoorebrian53@gmail.com>
2016-02-29 13:29:12 -0500

    url validation

--- server/resources/resourceController.js
+++ server/resources/resourceController.js
@@ -48,9 +48,15 @@ module.exports = {
   },
 
   addResource: function (req, res) {
-    var projectName = req.body.projectName;
-    if (!projectName) {
-      return res.end('projectName required');
+    var projectId = req.body.projectId;
+    var urlregex = /^(https?|ftp):\/\/([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])){3}|([a-zA-Z0-9-]+\.)*[a-zA-Z0-9-]+\.(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{2}))(:[0-9]+)*(\/($|[a-zA-Z0-9.,?'\\+&%$#=~_-]+))*$/;
+    var isValidUrl = urlregex.test(req.body.url);
+    if (!isValidUrl) {
+      console.log('bad url');
+      return res.end('Invalid url');
+    }
+    if (!projectId) {
+      return res.end('ProjectId required');
     }
     Project.findOne({ name: projectName })
     .then(function (project) {
